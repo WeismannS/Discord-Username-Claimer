5a717f72199421edfe0a64e1d3c69197b521786e
Weismann <60429301+WeismannS@users.noreply.github.com>
2023-05-04 12:48:17 +0100

    Fixed

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const token = "";
-const name = "";
-const headers = {"Content-Type": "application/json", "Authorization": token};
+const token = "NDMyMTU4NDE1NTI4Mzk0NzYy.G-YuXh.GNeSU2rO-e41KLlosvaZoy9fKo9NQTPAsnZYN4";
+const name = "Sahazel";
+const headers = {
+    "Content-Type": "application/json",
+    "Authorization": token
+};
 
 (async function validate() {
-    const res = await fetch("https://discord.com/api/v9/users/@me", {method: "GET", headers})
+    const res = await fetch("https://discord.com/api/v9/users/@me", {
+        method: "GET",
+        headers
+    })
     if (res.status !== 200) throw new Error("Invalid Token")
     else console.log("Token Validated!")
 })()
 const requested = async (timeout) => {
     const response = await fetch("https://discord.com/api/v10/users/@me/pomelo", {
-        method: "POST", body: JSON.stringify({username: name}), headers
+        method: "POST",
+        body: JSON.stringify({
+            username: name
+        }),
+        headers
     });
 
     if (response.status === 200) {
@@ -17,14 +27,17 @@ const requested = async (timeout) => {
         clearTimeout(timeout);
     } else if (response.status === 429) {
         console.log("%cGot Rate limited", "color:orange");
-        await new Promise((res) => setTimeout(()=>res("foo"),(response.headers.get("Retry-After")+2 ) *1000))
+        console.log(((+response.headers.get("Retry-After") + 2) * 1000))
+        await new Promise((res) => setTimeout(() => {
+            res("foo")
+        }, ((+response.headers.get("Retry-After") + 2) * 1000)))
     } else {
         console.log(`%c${response.statusText}`, "color:Red");
     }
 }
 
 (function recursive() {
-    const rand = Math.floor(Math.random() * (20000 - 3000 + 1)) + 3000;
+    const rand = Math.floor(Math.random() * (10000 - 3000 + 1)) + 3000;
     const timeout = setTimeout(async () => {
         await requested(timeout);
         recursive();
